214a80f30621904f5a6daeed66bcd2c8a6f3f4bb
saumya <saumyadudeja04@gmail.com>
2024-09-01 19:05:58 +0200

    put inventory_management in correct urls.py, add venv to gitignore

--- greenhome/greenhome/urls.py
+++ greenhome/greenhome/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('inventory_management/', include('inventory_management.urls')),
 ]
--- greenhome/urls.py
+++ greenhome/urls.py
@@ -3,5 +3,4 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('inventory_management/', include('inventory_management.urls')),
-]
+    ]
